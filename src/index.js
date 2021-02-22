c899415ff4d073a93b995baf5d3f66807f5a6706
yonatan.b <yonatanbenezra1@gmail.com>
2021-02-22 14:37:44 +0200

    fs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
-import App from 'App';
+import App from 'App/Index';
 
 ReactDOM.render(
   <CookiesProvider>
